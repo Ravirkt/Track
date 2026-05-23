bc20050080846864b1f5d4d08fb64e6360ab0d92
Ravirkt <r_tjikhoeri@live.com>
2026-05-23 17:48:09 +0200

    feat: added css logic #5

---
+++ src/lib/components/sections/Nav.svelte
@@ -0,0 +1,49 @@
+        pointer-events: auto;
+    }
+
+    @media (hover: hover) {
+        .nav-list:hover li:nth-child(1) {
+            transform: translateY(0px);
+        }
+        .nav-list:hover li:nth-child(2) {
+            transform: translateY(-49px);
+        }
+        .nav-list:hover li:nth-child(3) {
+            transform: translateY(-98px);
+        }
+        .nav-list:hover li:nth-child(4) {
+            transform: translateY(-147px);
+        }
+        .nav-list:hover li:nth-child(5) {
+            transform: translateY(-196px);
+        }
+        .nav-list:hover li:nth-child(6) {
+            transform: translateY(-245px);
+        }
+
+        .nav-list:hover li:not(:first-child) a {
+            pointer-events: auto;
+        }
+    }
+
+    .nav-item {
+        width: 3rem;
+        height: 3rem;
+        background-color: var(--card-background-color);
+        border-radius: var(--radius-sm);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+        cursor: pointer;
+        border: none;
+    }
+
+    .nav-item:hover {
+        background-color: var(--orange);
+    }
+
+    button.nav-item {
+        background-color: var(--card-background-color);
+    }
+</style>
