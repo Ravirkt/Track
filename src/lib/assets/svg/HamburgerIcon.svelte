f1d1868cf9fee17803bef15d5b2771627bfc2ee0
Ravirkt <r_tjikhoeri@live.com>
2026-05-23 17:49:38 +0200

    feat: added hamburger icon for nav #5

---
+++ src/lib/assets/svg/HamburgerIcon.svelte
@@ -0,0 +1,23 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-square-chevron-up-icon lucide-square-chevron-up"
+    ><rect width="18" height="18" x="3" y="3" rx="2" /><path
+        d="m8 14 4-4 4 4"
+    /></svg
+>
+
+<style>
+    svg {
+        flex-shrink: 0;
+        height: 20px;
+        width: 20px;
+        display: block;
+        color: hsl(0, 0%, 0%);
+    }
+</style>
