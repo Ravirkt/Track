74688effeec0893527af1b582513917dc3d9232f
Ravirkt <r_tjikhoeri@live.com>
2026-05-23 17:50:28 +0200

    feat: added nav component to layout file #5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,17 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import "$lib/assets/css/global.css";
 
+	    import { LoginIcon, } from "$lib";
+    import Nav from '$lib/components/sections/Nav.svelte';
+
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<Nav/>
 
 {@render children()}
