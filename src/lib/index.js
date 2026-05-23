9ccb93384475a9bf6b64e3e05493cfd58c5c32cb
Ravirkt <r_tjikhoeri@live.com>
2026-05-23 17:50:01 +0200

    feat:  import icons via index #5

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,13 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+
+
+
+// SVG
+export { default as LoginIcon } from "$lib/assets/svg/LoginIcon.svelte";
+export { default as HamburgerIcon } from "$lib/assets/svg/HamburgerIcon.svelte";
+
+
+
+// Components
+export { default as Nav } from "$lib/components/sections/Nav.svelte";
