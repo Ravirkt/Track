de683c6b879614aeb6b2b8e6e37c4d7b442e3a14
Ravirkt <r_tjikhoeri@live.com>
2026-05-26 17:20:57 +0200

    feat: added chartjs mini chart for the card as a atom component #14 #15

---
+++ src/lib/components/atoms/MiniChart.svelte
@@ -0,0 +1,52 @@
+<script>
+    import { onMount } from "svelte";
+    import { Chart } from "chart.js/auto";
+
+    let { data = [], labels = [] } = $props();
+
+    let canvas;
+
+    onMount(() => {
+        new Chart(canvas, {
+            type: "line",
+            data: {
+                labels,
+                datasets: [
+                    {
+                        data,
+                        borderColor: "black",
+                        borderWidth: 1.5,
+                        tension: 0.4,
+                        pointRadius: 3,
+                        pointBackgroundColor: "blue",
+                        pointBorderWidth: 0,
+                        fill: false,
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: { legend: { display: false } },
+                scales: {
+                    x: { display: false },
+                    y: { display: false },
+                },
+            },
+        });
+    });
+</script>
+
+<div class="mini-chart-container">
+    <canvas bind:this={canvas} />
+</div>
+
+<style>
+    .mini-chart-container {
+        width: 100%;
+    }
+
+    canvas {
+        width: 100%;
+    }
+</style>
