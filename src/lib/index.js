0e6096ba0142578db223415f16bd1232466fcda0
Ravirkt <r_tjikhoeri@live.com>
2026-05-26 17:23:41 +0200

    feat: import all assets and components via index file #14 #29

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,8 +6,22 @@
 // SVG
 export { default as LoginIcon } from "$lib/assets/svg/LoginIcon.svelte";
 export { default as HamburgerIcon } from "$lib/assets/svg/HamburgerIcon.svelte";
+export { default as CalenderIcon } from "$lib/assets/svg/CalenderIcon.svelte";
+export { default as DashboardIcon } from "$lib/assets/svg/DashboardIcon.svelte";
+export { default as UploadImageIcon } from "$lib/assets/svg/UploadImageIcon.svelte";
 
+
+export { default as TrophyIcon } from "$lib/assets/svg/TrophyIcon.svelte";
+export { default as WeightIcon } from "$lib/assets/svg/WeightIcon.svelte";
+export { default as WorkoutIcon } from "$lib/assets/svg/WorkoutIcon.svelte";
+
+export { default as ProgressPic } from "$lib/assets/img/ProgressPic.webp";
 
 
 // Components
 export { default as Nav } from "$lib/components/sections/Nav.svelte";
+export { default as MiniChart } from "$lib/components/atoms/MiniChart.svelte";
+export { default as ProgressForm } from "$lib/components/organisms/ProgressForm.svelte";
+export { default as Label } from "$lib/components/atoms/Label.svelte";
+export { default as HeaderTitle } from "$lib/components/atoms/HeaderTitle.svelte";
+export { default as HeaderSection } from "$lib/components/molecules/HeaderSection.svelte";
