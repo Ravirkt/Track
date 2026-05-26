543de3973bed6e343b639364f0fba8f2b544afe2
Ravirkt <r_tjikhoeri@live.com>
2026-05-26 17:22:19 +0200

    feat: updated the nav with final icons for each block #29

---
+++ src/lib/assets/svg/DashboardIcon.svelte
@@ -0,0 +1,36 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-layout-dashboard-icon lucide-layout-dashboard"
+    ><rect width="7" height="9" x="3" y="3" rx="1" /><rect
+        width="7"
+        height="5"
+        x="14"
+        y="3"
+        rx="1"
+    /><rect width="7" height="9" x="14" y="12" rx="1" /><rect
+        width="7"
+        height="5"
+        x="3"
+        y="16"
+        rx="1"
+    /></svg
+>
+
+
+
+
+<style>
+    svg {
+        flex-shrink: 0;
+        height: 20px;
+        width: 20px;
+        display: block;
+        color: var(--primary-text-color);
+    }
+</style>
