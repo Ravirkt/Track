d3a39d87e954619f300c0f106d8938481158f86e
Ravirkt <r_tjikhoeri@live.com>
2026-05-28 18:38:49 +0200

    feat: creating arrays for different values for the chart cards and formatting code and simple comments #14 #15 #16

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -15,6 +15,10 @@ export async function load() {
 
     const userData = await userResponse.json()
     console.log(userData)
+
+    // --------------------------
+
+    // fetch chart data
     const user = userData[0]
 
     const pictureResponse = await fetch(SUPABASE_URL + '/rest/v1/picture?user_id=eq.' + userId + '&select=weight,waist,body_fat,heart_rate&order=created_at.desc&limit=7', {
@@ -26,3 +30,14 @@ export async function load() {
 
     const pictureData = await pictureResponse.json()
     const pictures = pictureData
+
+    // create array for values
+    const weights = pictures.map(picture => picture.weight)
+    const waists = pictures.map(picture => picture.waist)
+    const bodyfats = pictures.map(picture => picture.body_fat)
+    const heartrates = pictures.map(picture => picture.heart_rate)
+
+
+    return { user, pictures, weights, waists, bodyfats, heartrates }
+
+}
