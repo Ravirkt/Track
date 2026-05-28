6ec51bc18845532d6128841a74f1359e48f75bfd
Ravirkt <r_tjikhoeri@live.com>
2026-05-28 18:28:28 +0200

    feat: added basic line chart as fallback when no values are inserted #14 #15

---
+++ src/lib/components/atoms/FallBackChart.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { onMount } from "svelte";
+    import { Chart } from "chart.js/auto";
+
+    let FallbackData = [65, 68, 64, 70, 67, 72, 69];
+    let Labels = ["1", "2", "3", "4", "5", "6", "7"];
+
+    let canvas;
+
+    onMount(() => {
+        new Chart(canvas, {
+            type: "line",
+            data: {
+                labels: dummyLabels,
+                datasets: [
+                    {
+                        data: FallbackData,
+                        borderColor: "grey",
+                        borderWidth: 1.5,
+                        tension: 0.4,
+                        pointRadius: 3,
+                        pointBackgroundColor: "grey",
+                        pointBorderWidth: 0,
+                        fill: false,
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+
+                plugins: {
+                    legend: { display: false },
+                    tooltip: { enabled: false },
+                },
+                scales: {
+                    x: { display: false },
+                    y: { display: false },
+                },
+            },
+        });
+    });
+</script>
+
+<canvas bind:this={canvas}></canvas>
+
+<style>
+    canvas {
+        width: 100%;
+    }
+</style>
