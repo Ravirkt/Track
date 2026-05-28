3b9ca50748e16a10d0f3024de2f362daef141ce8
Ravirkt <r_tjikhoeri@live.com>
2026-05-28 18:37:51 +0200

    feat: added fetch for chart data cards #14 #15 #16

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -3,6 +3,7 @@ import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private'
 // fetch user info
 export async function load() {
 
+    // for now manuel id
     const userId = 10
 
     const userResponse = await fetch(SUPABASE_URL + '/rest/v1/user?id=eq.' + userId + '&select=name,lastname,start_weight,start_bodyfat,start_waist,resting_heart_rate,created_at', {
@@ -14,3 +15,14 @@ export async function load() {
 
     const userData = await userResponse.json()
     console.log(userData)
+    const user = userData[0]
+
+    const pictureResponse = await fetch(SUPABASE_URL + '/rest/v1/picture?user_id=eq.' + userId + '&select=weight,waist,body_fat,heart_rate&order=created_at.desc&limit=7', {
+        headers: {
+            'apikey': SUPABASE_ANON_KEY,
+            'Authorization': SUPABASE_ANON_KEY,
+        }
+    })
+
+    const pictureData = await pictureResponse.json()
+    const pictures = pictureData
