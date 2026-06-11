3e157158b6f1875c73a51a098851330e93fb98f0
Ravirkt <r_tjikhoeri@live.com>
2026-06-11 16:50:38 +0200

    chore: formatting code #19

--- src/lib/components/organisms/ExerciseRow.svelte
+++ src/lib/components/organisms/ExerciseRow.svelte
@@ -1,11 +1,6 @@
 <script>
-    let {
-        allReps,
-        allSets,
-        headingTitleClass,
-        headingText,
-        headingLevel,
-    } = $props();
+    let { allReps, allSets, headingTitleClass, headingText, headingLevel } =
+        $props();
 
     import SetRepWrapper from "$lib/components/molecules/SetRepWrapper.svelte";
     import HeadingTitle from "../atoms/HeadingTitle.svelte";
@@ -29,6 +24,4 @@
         padding: 0.8rem;
         border-bottom: 2px solid black;
     }
-
-    
 </style>
